f8db46694d8fb8acb895f421e0a66b77601fd806
kieran <kieran@gmail.com>
2023-05-06 11:11:45 +0200

    add: graves

--- src/script.js
+++ src/script.js
@@ -60,21 +60,47 @@ const bushMaterial = new THREE.MeshStandardMaterial({color: '#89c854'});
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush1.scale.set(0.5, 0.5, 0.5);
-bush1.position.set(1, 0.3, 2.5)
+bush1.position.set(1, 0.3, 2.5);
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush2.scale.set(0.2, 0.2, 0.2);
-bush2.position.set(1.7, 0.15, 2.2)
+bush2.position.set(1.7, 0.15, 2.2);
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush3.scale.set(0.4, 0.4, 0.4);
-bush3.position.set(-1, 0.2, 2.4)
+bush3.position.set(-1, 0.2, 2.4);
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush4.scale.set(0.15, 0.15, 0.15);
-bush4.position.set(-1.45, 0.1, 2.6)
+bush4.position.set(-1.45, 0.1, 2.6);
 
-house.add(bush1, bush2, bush3, bush4)
+house.add(bush1, bush2, bush3, bush4);
+
+// Graves
+const graves = new THREE.Group();
+scene.add(graves);
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({color: "#b2b6b1"});
+
+for(let i=0; i<30; i++) {
+    // Create graves
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+
+    // Randomize positions
+    const angle = Math.random() * Math.PI * 2; // Angle
+    const radius = 3 + Math.random() * 6 // house's size (minimum) + random position until the end of the plane
+    const x = Math.cos(angle) * radius; // x axis
+    const z = Math.sin(angle) * radius; // z axis
+    grave.position.set(x, 0.3, z);
+
+    // Rotation
+    grave.rotation.y = (Math.random() - 0.5) * 0.4;
+    grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+    // Add the grave to the group
+    graves.add(grave);
+}
 
 // Floor
 const floor = new THREE.Mesh(
